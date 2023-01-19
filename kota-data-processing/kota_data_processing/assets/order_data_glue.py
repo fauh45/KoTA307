01c114a558cdc8db83e49c52b4c102a4a176cb22
Fauzan Lubis <fauzanfauh45@gmail.com>
2023-01-19 14:41:54 +0700

    fix(order-data-glue): cleanup spaces on strings for better filtering

--- kota-data-processing/kota_data_processing/assets/order_data_glue.py
+++ kota-data-processing/kota_data_processing/assets/order_data_glue.py
@@ -107,6 +107,9 @@ def cleaned_order_data_products(cleaned_raw_order_data: pd.DataFrame):
     df["Product description"] = df.apply(
         lambda x: _query_product_description(x["Lineitem sku"]), axis=1
     )
+    df["Product description"] = df["Product description"].apply(
+        lambda x: str(x).strip()
+    )
     df = df[~(df["Product description"] == "")]
 
     return df
