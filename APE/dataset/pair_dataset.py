7995519907527c43bcfb2cc6c3879ec5897d6c92
Fauzan Lubis <fauzanfauh45@gmail.com>
2023-03-06 23:21:44 +0700

    feat(dataset): created loader for generating pair data

---
+++ APE/dataset/pair_dataset.py
@@ -0,0 +1,36 @@
+from itertools import permutations
+import pandas as pd
+from torch.utils.data import Dataset
+
+
+class ProductOrderedPair(Dataset):
+    def __init__(self, cleaned_data: pd.DataFrame) -> None:
+        super().__init__()
+
+        self.dataset = self.permute_dataset(cleaned_data)
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        # get the row, flatten the result (to 1 dimension list) and convert it to a simple list
+        row = self.dataset.loc[index, :].values.flatten().tolist()
+
+        # [data, label]
+        return [row[:2], row[2]]
+
+    @staticmethod
+    def permute_dataset(cleaned_data: pd.DataFrame) -> pd.DataFrame:
+        grouped_training = cleaned_data.groupby("Email")
+
+        training_dataset_permuted = []
+        for group in grouped_training:
+            for b, g in permutations(group["Product description"].tolist(), 2):
+                # label are set to 1 on description that are not the same
+                # to denote that the data are positive pair (a user bought two of it)
+                training_dataset_permuted.append([b, g, 0 if b != g else 1])
+
+        return pd.DataFrame(
+            training_dataset_permuted,
+            columns=["description_1", "description_1", "label"],
+        ).reset_index(drop=True)
