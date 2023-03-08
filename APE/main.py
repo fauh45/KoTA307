49a70add22c1696fc4f7c4e2b2f3dc6f32a8540c
Fauzan Lubis <fauzanfauh45@gmail.com>
2023-03-08 15:24:46 +0700

    feat(main): script to interface with cli and run experiment

---
+++ APE/main.py
@@ -0,0 +1,72 @@
+import argparse
+import torch
+import sys
+import traceback
+
+from models.ELMo_pair_model import ELMoPairModel
+from models.BERT_pair_model import BERTPairModel
+from experiment import Experiment
+
+
+def start_experiment():
+    parser = argparse.ArgumentParser(description="Run Experiment")
+    parser.add_argument(
+        "--elmo-path",
+        type=str,
+        required=True,
+        help="Path where pretrained ELMo model are stored",
+    )
+    parser.add_argument(
+        "--bert-path",
+        type=str,
+        required=True,
+        help="Path or pretrained BERT identifier in Huggingface models hub",
+    )
+    parser.add_argument(
+        "--dataset-path",
+        type=str,
+        required=True,
+        help="Path of cleaned csv file",
+    )
+    parser.add_argument(
+        "--dry-run",
+        action=argparse.BooleanOptionalAction,
+        help="If the the experiment run is a dry run",
+    )
+
+    try:
+        args = parser.parse_args()
+
+        device = (
+            torch.device("cuda")
+            if torch.cuda.is_available()
+            else torch.device("cpu")
+        )
+        print(f"Device used: {device}")
+
+        elmo_model = ELMoPairModel(args.elmo_path)
+        bert_model = BERTPairModel(args.bert_path)
+
+        experiment = Experiment(
+            models=[elmo_model, bert_model],
+            dataset_path=args.dataset_path,
+            k_splits=10,
+            recommendation_amount=3,
+            min_product_bought=3,
+            epoch={5, 10, 15},
+            batch_size={8, 16, 32},
+            learning_rate={1e-5, 3e-5, 5e-5},
+            dry_run=args.dry_run,
+            save_dir="./experiment_output",
+        )
+
+        experiment.run_experiment()
+    except Exception:
+        traceback.print_exc()
+
+        parser.print_help()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    start_experiment()
