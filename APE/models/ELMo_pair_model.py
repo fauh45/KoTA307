1a3aef8014fc8f44fa09c8cdb2b86156625f04c5
Fauzan Lubis <fauzanfauh45@gmail.com>
2023-05-03 10:53:20 +0700

    fix(ELMo): Fix tensor detach from elmoformanylangs

--- APE/models/ELMo_pair_model.py
+++ APE/models/ELMo_pair_model.py
@@ -24,15 +24,12 @@ class ELMoPairModel(PairEmbeddingModel):
     def run_to_model_once(self, sentence_input: str):
         # TODO: Not really sure about this one, is it really updating the weights of the ELMo model?
         # Updated the sents2elmo using tensor instead, but still need to make sure
-        tensored = Variable(
-            self.__embedder.sents2elmo(
-                self.__split_description(sentence_input)
-            )[0],
-            requires_grad=True,
-        )
+        tensored = self.__embedder.sents2elmo(
+            self.__split_description(sentence_input)
+        )[0]
 
-        if self.gpu:
-            tensored = tensored.to("cuda")
+        if not self.gpu:
+            tensored = tensored.cpu()
 
         return tensored
 
