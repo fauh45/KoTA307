ff4ecbc19e4a9bc33f50f18985ba4aea4a6397fc
Fauzan Lubis <fauzanfauh45@gmail.com>
2023-06-06 15:29:56 +0700

    fix(training): removing nan checker

--- APE/models/model_training.py
+++ APE/models/model_training.py
@@ -108,13 +108,6 @@ class ModelTraining:
                 print("Loss value", loss)
                 print("Total loss", total_loss)
 
-            for name, param in self.model.named_parameters():
-                if torch.isnan(param.grad).any():
-                    print("nan gradient found!")
-
-                    if IS_DEBUG:
-                        raise SystemExit
-
             if batch_idx % 100 == 0:
                 print(f"\n\nLogging at {batch_idx}")
 
